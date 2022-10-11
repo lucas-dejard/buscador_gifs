b871e38c2af27694cf17382b63901a21c03fa04c
lucasdejard <erlucomlpg@gmail.com>
2022-10-11 12:02:21 -0300

    Added Lookup page and Share functionalities.

---
+++ android/app/src/main/kotlin/dejard/lucas/buscador_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package dejard.lucas.buscador_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:buscador_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
